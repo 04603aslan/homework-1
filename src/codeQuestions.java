644289a16f0d7f5ce5a5f949d296e32c06c120d7
Barış Karapelit <baris_karapelit_298@hotmail.com>
2020-06-11 16:36:29 +0300

    Ödevin Son Hali

--- src/codeQuestions.java
+++ src/codeQuestions.java
@@ -66,16 +66,55 @@ public class codeQuestions {
 					System.out.println("3<3<2");
 				}
 			}
-		}else if(number1==number3)
+		}
+		else if(number1==number3)
 		{
-			
+			if(number1<number2)
+			{
+				System.out.println("1.number=3.number<number2");
+			}
+			else if(number2<number1)
+			{
+				System.out.println("1.number=3.number>number2");
+			}
 		}
 		else if(number2==number3)
 		{
+			if(number1<number2)
+			{
+				System.out.println("1<3=2");
+			}
+			else if(number2<number1)
+			{
+				System.out.println("2=3<1");
+			}
 			
 		}
-
-
-	}
+		else if(number2<number1)
+		{
+			if(number1<number3)
+			{
+				System.out.println("2<1<3");
+			}
+			else if(number3<number1)
+			{
+				System.out.println("2<3<1");
+			}
+		}
+		else if (number3<number2)
+			
+		{
+			if(number1<number2)
+			{
+				System.out.println("3<1<2");
+			}
+			else if(number2<number1)
+			{
+				System.out.println("3<2<1");
+			}
+		}
+		
+		
+    }
 
 }
